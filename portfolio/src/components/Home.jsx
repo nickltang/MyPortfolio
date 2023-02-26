7c6c6c6b3214a4ec81f153265fcf0caa0c47d858
Nick <nicholasltang21@gmail.com>
2023-02-26 13:38:16 -0800

    removed unused components

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-[#8892b0]'>Hi, my name is</p>
-            <h1 className='text-4xl sm: text-7xl font-bold text-[#ccd6f6]'>Nick Tang</h1>
-            <h1 className='text-4xl sm: text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h1>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Nick Tang</h1>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h1>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>Blurb about my skills or whatever</p>
             <div>
                 <Link to="projects" smooth={true} duration={500}>
