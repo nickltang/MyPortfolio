472a92ae9e9b81730fd17004821d340fb16cfae4
Nick <nicholasltang21@gmail.com>
2023-03-09 14:57:42 -0800

    Added more projects

--- portfolio/src/data/data.js
+++ portfolio/src/data/data.js
@@ -1,17 +1,32 @@
 import Gdocs from '../assets/gdocs.png'
 import Lowkey from '../assets/lowkey.png'
+import Portfolio from '../assets/portfolio.png'
+import Stocked from '../assets/stocked.png'
 
 export const data = [
     {
         id: 1,
         name: 'QuizletDocs',
         image: Gdocs,
-        github: 'gdocs',
+        github: 'https://github.com/nickltang/QuizletDocs',
         live: ''
     },
-
     {
         id: 2,
+        name: 'Portfolio Website',
+        image: Portfolio,
+        github: 'https://github.com/nickltang/MyPortfolio',
+        live: ''
+    },
+    {
+        id: 3,
+        name: 'Stocked',
+        image: Stocked,
+        github: 'https://github.com/Kevin-Benelli/2021-fall-cs160-pied-piper',
+        live: ''
+    },
+    {
+        id: 4,
         name: 'Lowkey',
         image: Lowkey,
         github: 'https://github.com/nickltang/low-key',
